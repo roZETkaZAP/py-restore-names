f6a3860eb2368eadbd4d2e6ca1e5a73d8f043d9f
Igor <zalugin.iv2@gmail.com>
2026-04-25 23:09:36 +0200

    all tests written

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,19 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+def test_if_first_name_not_in_user() -> None:
+    users = [{"full_name": "John Smith"}]
+    restore_names(users)
+    assert users[0]["first_name"] == "John"
+
+
+def test_restore_first_name_when_none():
+    users = [{"full_name": "Alice Cooper", "first_name": None}]
+    restore_names(users)
+    assert users[0]["first_name"] == "Alice"
+
+
+def test_first_name_in_user() -> None:
+    users = [{"full_name": "Kyle Smith", "first_name": "Kyle"}]
+    restore_names(users)
+    assert users[0]["first_name"] == "Kyle"
