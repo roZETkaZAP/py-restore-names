d23f59591595c7611ecc80d35f7161ad6a50c131
Igor <zalugin.iv2@gmail.com>
2026-04-25 23:12:06 +0200

    flake8 changes

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -7,7 +7,7 @@ def test_if_first_name_not_in_user() -> None:
     assert users[0]["first_name"] == "John"
 
 
-def test_restore_first_name_when_none():
+def test_restore_first_name_when_none() -> None:
     users = [{"full_name": "Alice Cooper", "first_name": None}]
     restore_names(users)
     assert users[0]["first_name"] == "Alice"
